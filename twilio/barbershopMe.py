efefa8b89159e1f2d9ac0b8ba9297a9a58a6b3b1
Nick Palmer <nick@palmr.co.uk>
2012-11-17 21:34:57 +0000

    grab audio

--- twilio/barbershopMe.py
+++ twilio/barbershopMe.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python
-from datetime import datetime 
-print(str(datetime.now()) + "\n")
-
 import cgi
-import cgitb
-cgitb.enable()
-form = cgi.FieldStorage()
 
-for k in form.keys():
-  print( "k: " + str(k) + "v: " + str(form[k]))
+#import cgitb
+#cgitb.enable()
+
+inputs = cgi.FieldStorage()
+
+
+
+# Parameters
+
+# RecordingUrl 	the URL of the recorded audio
+# RecordingDuration 	the duration of the recorded audio (in seconds)
+# Digits 	the key (if any) pressed to end the recording or 'hangup' if the caller hung up
+
+audioURL = inputs['RecordingURL']
+
+localAudioFile = "testAudio_" + str(datetime.now()) + ".wav"
+urllib.urlretrieve(audioURL, localAudioFile)
+
+print ("Retrieved " + audioURL + "\n")
