c3063cf64b4271a2ae63af9f9de37824515fc3fc
Nick Palmer <nick@palmr.co.uk>
2012-11-17 21:01:00 +0000

    use cgi bin

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,2 @@
 cp twilio/recordSong.twiml /var/www/html/music-hack-day/recordSong.twiml
-cp twilio/barbershopMe.py /var/www/html/music-hack-day/barbershopMe.py
+cp twilio/barbershopMe.py /var/www/cgi-bin/music-hack-day/barbershopMe.py
