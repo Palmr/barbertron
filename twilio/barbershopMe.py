3ceb1622b9a2c622af9b6722163064c597a7b904
Nick Palmer <nick@palmr.co.uk>
2012-11-18 00:20:58 +0000

    Finally reading the value from the field :D

--- twilio/barbershopMe.py
+++ twilio/barbershopMe.py
@@ -8,19 +8,18 @@ import urllib
 from datetime import datetime
 inputs = cgi.FieldStorage()
 
+print("Content-type: text/plain\n\n")
 
 with open("/var/www/cgi-bin/music-hack-day/log_"+ str(datetime.now()) + ".log","w+") as logFile:
   logFile.write("HEADERS:\n ")
   logFile.write(str(inputs))
 
-
 # Parameters
-
 # RecordingUrl 	the URL of the recorded audio
 # RecordingDuration 	the duration of the recorded audio (in seconds)
 # Digits 	the key (if any) pressed to end the recording or 'hangup' if the caller hung up
 
-audioURL = str(inputs['RecordingUrl'])
+audioURL = str(inputs['RecordingUrl'].value)
 
 localAudioFile = "testAudio_" + str(datetime.now()) + ".wav"
 urllib.urlretrieve(audioURL, localAudioFile)
