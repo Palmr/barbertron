7538de28eb366d105b95aacf6db51c4179c314b0
Nick Palmer <nick@palmr.co.uk>
2012-11-18 13:23:16 +0000

    fix

--- twilio/barbershop.py
+++ twilio/barbershop.py
@@ -16,13 +16,15 @@ def generateControlFile(inputFile, barbershopID):
   with open(controlFilename, "w+") as controlFile:
 
     # test, go up in whole tones
-    for t in xrange(0,10):
-      controlFile.write("%f %i %i\n" % (t/2.0, 64 + 2 * t, 127 ))
+    for t in xrange(1,4):
+      controlFile.write("%f %i %i\n" % (t*2.0, 54 + 3 * t, 117 ))
+      controlFile.write("%f %i %i\n" % (t*2.0, 59 + 3 * t, 117 ))
+      controlFile.write("%f %i %i\n" % (t*2.0, 64 + 3 * t, 117 ))
   
   return controlFilename
 
 
 def doBarberShopping(inputFile, outputFile, barbershopID):
   controlFilename = generateControlFile(inputFile, barbershopID)
-  subprocess.call(["./barberism", "patch.pd", inputFile, controlFilename, outputFile])
+  subprocess.call(["./barberism", "./patch.pd", inputFile, controlFilename, outputFile])
 
